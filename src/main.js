81b67e4f42fce10c758c7c4234e3f94d701aa226
jjaekong <jjaekong@gmail.com>
2022-07-11 14:10:16 +0900

    페이지 구성 테스트

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import './style.css';
+import App from './App.vue';
+import Home from './views/Home.vue';
+import Me from './views/Me.vue';
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: Home },
+    { path: 'me', component: Me },
+  ],
+});
+
+createApp(App).use(router).mount('#app');

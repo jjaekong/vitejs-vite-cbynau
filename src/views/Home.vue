232f649d245c4c5ba245f2edd17d054b261dcacb
jjaekong <jjaekong@gmail.com>
2022-07-13 07:32:30 +0900

    me 페이지 제거, home 에서 드롭다운으로 변경

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,31 +13,22 @@ export default {
 <template>
   <div id="home" role="document">
     <header>
-      <h1 class="logo">미땅고</h1>
+      <h1 class="logo">LG</h1>
       <router-link class="settings" to="/settings"
         ><i class="bi bi-gear"></i
       ></router-link>
-      <div class="dropdown">
-        <a
-          class="btn btn-secondary dropdown-toggle"
-          href="#"
-          role="button"
-          id="dropdownMenuLink"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Dropdown link
+      <div class="dropdown ms-auto" v-if="store.user">
+        <a href="#" class="avatar" data-bs-toggle="dropdown">
+          <img :src="store.user.photoURL" :alt="store.user.displayName" />
         </a>
 
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+        <ul class="dropdown-menu shadow" aria-labelledby="dropdownMenuLink">
           <li><a class="dropdown-item" href="#">Action</a></li>
           <li><a class="dropdown-item" href="#">Another action</a></li>
-          <li><a class="dropdown-item" href="#">Something else here</a></li>
+          <li><hr class="dropdown-divider" /></li>
+          <li><a class="dropdown-item" href="#">로그아웃</a></li>
         </ul>
       </div>
-      <router-link class="avatar" to="/me" v-if="store.user">
-        <img :src="store.user.photoURL" :alt="store.user.displayName" />
-      </router-link>
       <router-link class="avatar" to="/login" v-else>LOGIN</router-link>
     </header>
   </div>
@@ -46,29 +37,29 @@ export default {
 <style scoped lang="scss">
 #home {
   padding: 1rem;
-}
-header {
-  display: flex;
-  align-items: center;
-  .logo {
-    margin: 0;
-    font-weight: 600;
-    letter-spacing: -0.1rem;
-  }
-  .country {
-    margin-left: 0.5rem;
-  }
-  .settings {
-    margin-left: 1rem;
-    color: var(--bs-gray-600);
-    font-size: 1.5rem;
-  }
-  .avatar {
-    margin-left: auto;
-    img {
-      width: 2.5rem;
-      height: 2.5rem;
-      border-radius: 50%;
+  header {
+    display: flex;
+    align-items: center;
+    .logo {
+      margin: 0;
+      font-weight: 600;
+      letter-spacing: -0.1rem;
+    }
+    .country {
+      margin-left: 0.5rem;
+    }
+    .settings {
+      margin-left: 1rem;
+      color: var(--bs-gray-600);
+      font-size: 1.5rem;
+    }
+    .avatar {
+      margin-left: auto;
+      img {
+        width: 2.5rem;
+        height: 2.5rem;
+        border-radius: 50%;
+      }
     }
   }
 }

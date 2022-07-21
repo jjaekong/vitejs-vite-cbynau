7073a3992bd29c19c8103959a907e512cb8e08b8
jjaekong <jjaekong@gmail.com>
2022-07-21 18:34:46 +0900

    프로필 수정 UI/로직 수정

--- src/components/EditProfileModal.vue
+++ src/components/EditProfileModal.vue
@@ -11,13 +11,13 @@ export default {
       photo: null,
       name: null,
       file: null,
-      isOrganizer: false,
+      isOrganizer: true,
       uploadPhotoValid: null,
       uploadPhotoFeedback: null,
-      updateNameValid: null,
-      updateNameFeedback: null,
-      updateOrgValid: null,
-      updateOrgFeedback: null,
+      saveNameValid: null,
+      saveNameFeedback: null,
+      saveRoleOrgValid: null,
+      saveRoleOrgFeedback: null,
     };
   },
   computed: {
@@ -25,6 +25,7 @@ export default {
   },
   watch: {
     user: function () {
+      console.log('watch user', this.user);
       this.fetchUserData();
     },
   },
@@ -39,21 +40,12 @@ export default {
   methods: {
     fetchUserData() {
       this.name = !this.user ? null : this.user.displayName;
-      this.isOrganizer = !this.user ? null : this.user.isOrganizer;
-    },
-    editProfile() {
-      // const auth = getAuth();
-      // const db = getFirestore();
-      // const userRef = doc(
-      //   db,
-      //   import.meta.env.PROD ? 'users' : 'dev_users',
-      //   this.user.uid
-      // );
+      this.isOrganizer = !this.user ? false : this.user.isOrganizer;
     },
     uploadPhoto() {
       console.log('upload photo');
     },
-    updateName() {
+    saveName() {
       if (this.name) {
         if (this.name !== this.user.displayName) {
           const auth = getAuth();
@@ -61,38 +53,54 @@ export default {
             displayName: this.name,
           })
             .then(() => {
-              this.updateNameValid = true;
-              this.updateNameFeedback = '이름(닉네임)이 변경되었습니다.';
+              this.saveNameValid = true;
+              this.saveNameFeedback = '이름(닉네임)이 변경되었습니다.';
               this.store.setUser({ displayName: this.name });
             })
             .catch((error) => {
-              this.updateNameValid = false;
-              this.updateNameFeedback =
+              this.saveNameValid = false;
+              this.saveNameFeedback =
                 '이름(닉네임)을 변경하는데 오류가 발생했습니다.';
             });
         } else {
-          this.updateNameValid = false;
-          this.updateNameFeedback = '기존 이름(닉네임)과 동일합니다.';
+          this.saveNameValid = false;
+          this.saveNameFeedback = '기존 이름(닉네임)과 동일합니다.';
         }
       } else {
-        this.updateNameValid = false;
-        this.updateNameFeedback = '이름(닉네임)을 입력하세요.';
+        this.saveNameValid = false;
+        this.saveNameFeedback = '이름(닉네임)을 입력하세요.';
       }
     },
-    updateOrgRole() {
-      this.updateOrgValid = true;
-      this.updateOrgFeedback = `오거나이저 역할을 ${
-        this.isOrganizer ? '' : '비'
-      }활성화했습니다.`;
+    saveRole() {
+      if (this.isOrganizer !== this.user.isOrganizer) {
+        const db = getFirestore();
+        const userRef = doc(
+          db,
+          import.meta.env.PROD ? 'users' : 'dev_users',
+          this.user.uid
+        );
+        setDoc(userRef, { isOrganizer: this.isOrganizer }, { merge: true })
+          .then(() => {
+            this.saveRoleOrgValid = true;
+            this.saveRoleOrgFeedback = `오거나이저 역할을 ${
+              this.isOrganizer ? '' : '비'
+            }활성화했습니다.`;
+            this.store.setUser({ isOrganizer: this.isOrganizer });
+          })
+          .catch((error) => {
+            this.saveRoleOrgValid = false;
+            this.saveRoleOrgFeedback = `역할을 저장하는데 오류가 발생했습니다.`;
+          });
+      }
     },
     resetValidData() {
       console.log('resetValidData');
       this.uploadPhotoValid = null;
       this.uploadPhotoFeedback = null;
-      this.updateNameValid = null;
-      this.updateNameFeedback = null;
-      this.updateOrgValid = null;
-      this.updateOrgFeedback = null;
+      this.saveNameValid = null;
+      this.saveNameFeedback = null;
+      this.saveRoleOrgValid = null;
+      this.saveRoleOrgFeedback = null;
     },
   },
 };
@@ -131,14 +139,14 @@ export default {
                 @input="uploadPhoto"
               />
             </div>
-            <div class="mb-3">
+            <div class="mb-4">
               <label class="form-label" for="name">이름 또는 닉네임:</label>
               <div
                 class="input-group"
                 :class="
-                  updateNameValid === null
+                  saveNameValid === null
                     ? null
-                    : updateNameValid
+                    : saveNameValid
                     ? 'is-valid'
                     : 'is-invalid'
                 "
@@ -150,9 +158,9 @@ export default {
                   v-model="name"
                   required
                   :class="
-                    updateNameValid === null
+                    saveNameValid === null
                       ? null
-                      : updateNameValid
+                      : saveNameValid
                       ? 'is-valid'
                       : 'is-invalid'
                   "
@@ -160,61 +168,74 @@ export default {
                 <button
                   type="button"
                   class="btn btn-primary"
-                  @click="updateName"
+                  @click="saveName"
+                  :disabled="this.name === this.user.displayName"
                 >
-                  변경
+                  저장
                 </button>
               </div>
               <div
                 :class="
-                  updateNameValid === null
+                  saveNameValid === null
                     ? null
-                    : updateNameValid
+                    : saveNameValid
                     ? 'valid-feedback'
                     : 'invalid-feedback'
                 "
               >
-                {{ updateNameFeedback }}
+                {{ saveNameFeedback }}
               </div>
             </div>
-            <div class="mb-3">
-              <h6 class="form-label">역할</h6>
-              <div class="form-check form-switch">
-                <input
-                  class="form-check-input"
-                  :class="
-                    updateOrgValid === null
-                      ? null
-                      : updateOrgValid
-                      ? 'is-valid'
-                      : 'is-invalid'
-                  "
-                  type="checkbox"
-                  role="switch"
-                  id="is-organizer"
-                  v-model="isOrganizer"
-                  @change="updateOrgRole"
-                />
-                <label class="form-check-label" for="is-organizer"
-                  >오거나이저</label
-                >
-                <div
-                  :class="
-                    updateOrgValid === null
-                      ? null
-                      : updateOrgValid
-                      ? 'valid-feedback'
-                      : 'invalid-feedback'
-                  "
-                >
-                  {{ updateOrgFeedback }}
+          </form>
+          <form @submit.prevent="saveRole">
+            <fieldset>
+              <legend>
+                역할 {{ this.isOrganizer }} {{ this.user.isOrganizer }}
+              </legend>
+              <div class="mb-3">
+                <div class="form-check form-switch">
+                  <input
+                    class="form-check-input"
+                    :class="
+                      saveRoleOrgValid === null
+                        ? null
+                        : saveRoleOrgValid
+                        ? 'is-valid'
+                        : 'is-invalid'
+                    "
+                    type="checkbox"
+                    role="switch"
+                    id="is-organizer"
+                    v-model="isOrganizer"
+                  />
+                  <label class="form-check-label" for="is-organizer"
+                    >오거나이저</label
+                  >
+                  <div
+                    :class="
+                      saveRoleOrgValid === null
+                        ? null
+                        : saveRoleOrgValid
+                        ? 'valid-feedback'
+                        : 'invalid-feedback'
+                    "
+                  >
+                    {{ saveRoleOrgFeedback }}
+                  </div>
+                </div>
+                <div class="form-text">
+                  오거나이저 역할을 활성화하면, 밀롱가를 생성하고 밀롱가
+                  이벤트를 추가할 수 있습니다.
                 </div>
               </div>
-              <div class="form-text">
-                오거나이저 역할을 활성화하면, 밀롱가를 생성하고 밀롱가 이벤트를
-                추가할 수 있습니다.
-              </div>
-            </div>
+              <button
+                type="submit"
+                class="btn btn-primary"
+                :disabled="this.isOrganizer === this.user.isOrganizer"
+              >
+                역할 저장
+              </button>
+            </fieldset>
           </form>
         </div>
       </div>
